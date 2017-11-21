06715feaceab3d248c2460553e9b92a6f00abc56
Owen Rumney <owen@owenrumney.co.uk>
2017-11-21 15:02:50 +0000

    Updates

--- hbase-1.2.0/install-hbase.sh
+++ hbase-1.2.0/install-hbase.sh
@@ -4,3 +4,8 @@ wget --quiet http://archive.apache.org/dist/hbase/1.2.0/hbase-1.2.0-bin.tar.gz
 tar xzf hbase-1.2.0-bin.tar.gz -C /opt/
 ln -s /opt/hbase-1.2.0 /opt/hbase
 rm hbase-1.2.0-bin.tar.gz
+
+# install the snappy libraries
+wget --quiet http://archive.apache.org/dist/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
+tar -zxf hadoop-2.7.1.tar.gz -C /tmp
+mv /tmp/hadoop-2.7.1/lib/* $HBASE_HOME/lib/
